41c9789f125459a7c6ee993698f4e921845bbf2c
Helmy Kambuna Otaya (Tokko Blogger) <21377149+twinnybloggers@users.noreply.github.com>
2023-08-28 08:43:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -40,11 +40,7 @@ INITIALIZE
 document.addEventListener('DOMContentLoaded', function() {
 
   // FIREBASE CONFIG
-  var config = {
-    apiKey: "your api key goes here",
-    authDomain: "your auth domain",
-    databaseURL: "your db url",
-    storageBucket: "your bucket",
+  var config = {apiKey: "AIzaSyC-SEma3JShAJXe4h47x9nWPMRf8aF9e7M", authDomain: "tokko-login.firebaseapp.com", databaseURL: "https://tokko-login-default-rtdb.firebaseio.com", projectId: "tokko-login", storageBucket: "tokko-login.appspot.com", messagingSenderId: "604336992026", appId: "1:604336992026:web:8091ec5a0b8a1a5d15c6a3", measurementId: "G-VYVZGNYLC3",
   };
   
   //INITIALIZE FIREBASE WEB APP
